0d7893218fdea43f894a2fea622fe3e1beeec584
jack_zhao <jack_zhao@trendmicro.com.cn>
2021-09-27 10:01:44 +0800

    add check init

--- simple_kv/src/main/java/com/jackzhao/simple_kv/SimpleKvMgr.kt
+++ simple_kv/src/main/java/com/jackzhao/simple_kv/SimpleKvMgr.kt
@@ -6,7 +6,11 @@ object SimpleKvMgr {
     var type: StorageType = StorageType.MMKV
     var context: Context? = null
 
+    @Synchronized
     fun init(context: Context, type: StorageType = StorageType.MMKV) {
+        if (context != null) {
+            return
+        }
         this.type = type
         this.context = context
     }
